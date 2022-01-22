3c932b3f6dbfddcbcfddc276d6d5f3581ff56742
Michal Dymerski <michal.dymerski.dev@gmail.com>
2022-01-22 21:26:02 +0100

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-console.log("witam w moim skrypcie");
-let bt=document.querySelector(".bt");
-let yt=document.querySelector(".yt");
-bt.addEventListener("click", () => {
-    yt.remove();
-    window.open("https://youtu.be/ca90ini4jM4")
+console.log("Hi!");
+let button=document.querySelector(".button");
+let body=document.querySelector(".body");
+let nav=document.querySelector(".navigation");
+let main=document.querySelector(".main");
+let header=document.querySelector(".header");
+let footer=document.querySelector(".footer");
+let headerIn=document.querySelector(".headerIn");
+let table=document.querySelector(".table");
+
+button.addEventListener("click", () => {
+    headerIn.classList.toggle("matrix");
+    body.classList.toggle("matrix");
+    main.classList.toggle("matrix");
+    header.classList.toggle("matrix");
+    nav.classList.toggle("matrix");
+    footer.classList.toggle("matrix");
+    table.classList.toggle("matrix");
+    button.innerText=body.classList.contains("matrix")? "NORMAL" : "MATRIX";
 } );
